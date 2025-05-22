20ece9747cb8c53cc4fd075599cf66c98c9556c0
Sofie <Soov0001@stud.kea.dk>
2025-05-22 16:00:28 +0200

    meta

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import About from "./components/About";
-import Cv from "./components/Cv";
+import Head from "next/head";
+
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <meta name="robots" content="noindex, nofollow" />
+      </Head>
       <div className="h-screen">
         <Header></Header>
         <Hero></Hero>
